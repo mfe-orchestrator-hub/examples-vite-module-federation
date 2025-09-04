9e81e1cf7b93686f35fda049f3ed49bbfd425aa2
Ilaria <ilaria.ciavarella@hotmail.com>
2025-09-04 19:01:14 +0200

    test

--- remote/src/components/Button.jsx
+++ remote/src/components/Button.jsx
@@ -6,10 +6,15 @@ import { useState } from "react"
 export const Button = () => {
   const [state, setState] = useState(0)
   return (
-    <div>
-      <button id='click-btn' className='shared-btn' onClick={() => setState((s) => s + 1)}>Click me: {state}</button>
-    </div>
-  )
+		<div>
+			<button
+				id="click-btn"
+				className="shared-btn"
+				onClick={() => setState((s) => s + 1)}>
+				Click me: {state}
+			</button>
+		</div>
+	);
 }
 
 export default Button
